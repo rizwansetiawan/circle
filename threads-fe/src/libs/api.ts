61abc0d3ca8e633c810eb301f3d1a5a2790cdfe2
rizwansetiawan <rizwandhoel@gmail.com>
2023-11-06 19:05:23 +0700

    docs: circle app version 0.2

--- threads-fe/src/libs/api.ts
+++ threads-fe/src/libs/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1"
+    baseURL: "https://circle-tan.vercel.app/api/v1"
 })
 
 export function setAuthToken(token: string) {
