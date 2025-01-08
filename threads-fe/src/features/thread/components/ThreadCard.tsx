fc2bbcc1eb914871a37f2af4a8a88f81508afc09
Rizwan Setiawan <rizwansetiawan@Rzn-MacBook-Air.local>
2025-01-08 17:11:17 +0700

    add well text

--- threads-fe/src/features/thread/components/ThreadCard.tsx
+++ threads-fe/src/features/thread/components/ThreadCard.tsx
@@ -12,75 +12,69 @@ export function ThreadCard(props: IThreadCard) {
   const { handlePostLike } = useThreadCard();
   const [showImage, setImage] = useState<boolean>(true);
   return (
-      <Box
-        display={"flex"}
-        width="100%"
-        padding={"20px"}
-      >
-        <Avatar
-          src={props.user?.picture}
-          width={"50px"}
-          height={"50px"}
-          objectFit={"cover"}
-          marginRight={"20px"}
-        />
+    <Box display={"flex well"} width="100%" padding={"20px"}>
+      <Avatar
+        src={props.user?.picture}
+        width={"50px"}
+        height={"50px"}
+        objectFit={"cover"}
+        marginRight={"20px"}
+      />
 
-        <Box display={"flex"} flexDirection={"column"}>
-          <Box
-          >
-            <Box display={"flex"} flexDirection={"column"} gap={2}>
-              <Box display={"flex"} gap={5}>
-                <Text>{props.user?.full_name}</Text>
-                <Text color="brand.grey" display={"flex"}>
-                  @{props.user?.username}{" "}
-                  <RxDotFilled style={{ marginTop: "5px" }} />
-                  {moment(props.posted_at).startOf("minute").fromNow()}
-                </Text>
-              </Box>
-              <Text>{props.content}</Text>
-              {showImage && (
-                <Image
-                  src={props.image}
-                  cursor={"pointer"}
-                  objectFit={"contain"}
-                  alt="user"
-                  onClick={() => navigate(`/detail/${props.id}`)}
-                  onError={() => setImage(false)}
-                />
-              )}
-            </Box>
-          </Box>
-
-          <Box
-            display={"flex"}
-            gap={5}
-            marginTop={"10px"}
-            justifyContent={"space-between"}
-          >
-            <Button
-              pl={0}
-              gap={"2"}
-              variant="none"
-              onClick={() => handlePostLike(props.id, props.is_liked)}
-            >
-              {props.is_liked ? (
-                <AiFillHeart style={{ fontSize: "1.5em", color: "red" }} />
-              ) : (
-                <AiOutlineHeart style={{ fontSize: "1.5em", color: "white" }} />
-              )}
-              <Text color="brand.grey" ml={"5px"}>
-                {props.likes_count} Like
+      <Box display={"flex"} flexDirection={"column"}>
+        <Box>
+          <Box display={"flex"} flexDirection={"column"} gap={2}>
+            <Box display={"flex"} gap={5}>
+              <Text>{props.user?.full_name}</Text>
+              <Text color="brand.grey" display={"flex"}>
+                @{props.user?.username}{" "}
+                <RxDotFilled style={{ marginTop: "5px" }} />
+                {moment(props.posted_at).startOf("minute").fromNow()}
               </Text>
-            </Button>
-            <Link to={`/detail/${props.id}`}>
-            <Button pl={0} variant="none" gap={"2"}  >
+            </Box>
+            <Text>{props.content}</Text>
+            {showImage && (
+              <Image
+                src={props.image}
+                cursor={"pointer"}
+                objectFit={"contain"}
+                alt="user"
+                onClick={() => navigate(`/detail/${props.id}`)}
+                onError={() => setImage(false)}
+              />
+            )}
+          </Box>
+        </Box>
+
+        <Box
+          display={"flex"}
+          gap={5}
+          marginTop={"10px"}
+          justifyContent={"space-between"}
+        >
+          <Button
+            pl={0}
+            gap={"2"}
+            variant="none"
+            onClick={() => handlePostLike(props.id, props.is_liked)}
+          >
+            {props.is_liked ? (
+              <AiFillHeart style={{ fontSize: "1.5em", color: "red" }} />
+            ) : (
+              <AiOutlineHeart style={{ fontSize: "1.5em", color: "white" }} />
+            )}
+            <Text color="brand.grey" ml={"5px"}>
+              {props.likes_count} Like
+            </Text>
+          </Button>
+          <Link to={`/detail/${props.id}`}>
+            <Button pl={0} variant="none" gap={"2"}>
               <AiOutlineComment style={{ fontSize: "1.5em" }} />
               <Text color="brand.grey">{props.replies_count} Replies</Text>
             </Button>
-            </Link>
-          </Box>
+          </Link>
         </Box>
       </Box>
-
+    </Box>
   );
 }
